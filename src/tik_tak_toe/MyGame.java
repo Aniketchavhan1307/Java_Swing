3a095b4e6bc7fab8a3881678cdaeb8880a6378d5
Aniket <saniketchavhan123@gmail.com>
2025-02-05 01:08:01 +0530

    added images

--- src/tik_tak_toe/MyGame.java
+++ src/tik_tak_toe/MyGame.java
@@ -1,9 +1,25 @@
 package tik_tak_toe;
 
-public class MyGame 
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+public class MyGame extends JFrame
 {
 	
 	public MyGame() 
 	{
+		setTitle("My Tic Tac Toe Game");
+		setSize(850,850);
+		
+		ImageIcon icon = new ImageIcon("src\\img\\xo.jpg");
+		
+		setIconImage(icon.getImage());
+		
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		setVisible(true);
 	}
-}
+	
+	
+	 
+} 
